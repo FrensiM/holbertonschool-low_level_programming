cb3a79603c99f775fbbfd55f06990744256e705b
FrensiM <frensimeta@gmail.com>
2023-02-17 02:31:56 -0800

    size is not grandeur

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-printf("size of a char: %ld byte(s)\n", sizeof(char));
+printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %ld byte(s)\n", sizeof(int));
 printf("size of a long int: %ld byte(s)\n", sizeof(long);
 printf("size of a long long int: %ld byte(s)\n", sizeof(long long);
