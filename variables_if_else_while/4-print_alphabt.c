d7a9e246f7be87f12d01305c3ed378c0bb2c08a4
FrensiM <frensimeta@gmail.com>
2023-02-20 04:44:27 -0800

    when i was having that alphabet soup

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+if (letter != 'q' && letter != 'e')
+{
+putchar(letter);
+}
+letter++;
+}
+putchar('\n');
+return (0);
+}
