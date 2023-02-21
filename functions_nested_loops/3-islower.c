01428c255facc5feba8400fced4b5d13ca819f7e
FrensiM <frensimeta@gmail.com>
2023-02-21 04:55:41 -0800

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int _islower(int c)
+{	
+	if(c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+		c++;
+	}
+return (0);
+}
