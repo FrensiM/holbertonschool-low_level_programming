0892c918945776e0105e16731fa2679b4aad91d3
FrensiM <frensimeta@gmail.com>
2023-02-21 00:36:19 -0800

    patience

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,9 +8,13 @@ int main(void)
 {
 int i;
 for (i = '0'; i <= '9'; i++)
-putchar(i);
-putchar(',');
-putchar(' ');
+{
+	putchar((i % 10) + '0');
+	if (i == 9)
+		continue;
+	putchar(',');
+	putchar(' ');
+}
 putchar('\n');
 return (0);
 }
