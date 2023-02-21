2666e0d394e71080e598e4a84b4db0db7f0e33c3
FrensiM <frensimeta@gmail.com>
2023-02-21 05:03:40 -0800

    is lower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* main - Entry point
-*
+* _islower - main function
+* @c: Parameter for letters
 * Return: Always 0 (success)
 */
 int _islower(int c)
-{	
-	if(c >= 'a' && c <= 'z')
+{
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
