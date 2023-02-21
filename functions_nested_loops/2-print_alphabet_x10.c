ccfd7e4928c2db50f018e95c28b44a50e6720982
FrensiM <frensimeta@gmail.com>
2023-02-21 04:41:16 -0800

    10x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,10 @@ void print_alphabet_x10(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		char letter = 'a';
+
 		while (letter <= 'z')
 		{
 			_putchar(letter);
