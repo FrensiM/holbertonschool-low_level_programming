52c8b93f88c43115402e3ebdc65e42e4accfca47
FrensiM <frensimeta@gmail.com>
2023-02-21 02:31:39 -0800

    i sometimes suffer from insomnia

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,16 @@
 #include "main.h"
-
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
 void print_alphabet(void)
 {
 char letter = 'a';
 while (letter <= 'z')
 {
-	putchar(letter);
-	letter ++
+	_putchar(letter);
+	letter++;
 }
-putchar('\n');
+_putchar('\n');
 }
