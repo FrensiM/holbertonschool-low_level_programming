26a183bc86a7dc8e752dc571707be41058032233
FrensiM <frensimeta@gmail.com>
2023-02-22 03:15:34 -0800

    learn your ties table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,32 +6,33 @@
  */
 void times_table(void)
 {
-int i, j;
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-int product = i * j;
-if (product < 10)
-{
-_putchar(' ');
-_putchar(product + '0');
-}
-else if (product < 100)
-{
-_putchar(' ');
-_putchar((product / 10) + '0');
-_putchar((product % 10) + '0');
-}
-else
-{
-_putchar((product / 100) + '0');
-_putchar(((product % 100) / 10) + '0');
-_putchar((product % 10) + '0');
-}
-_putchar(',');
-_putchar(' ');
-}
-_putchar('\n');
-}
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int product = i * j;
+
+			if (j == 0)
+			{
+				_putchar(product + '0');
+			}
+			if (product < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else if (product >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
