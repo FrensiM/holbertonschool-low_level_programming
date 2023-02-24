2c31ccb62f77cf3752f10686c27031ff71baeda0
FrensiM <frensimeta@gmail.com>
2023-02-24 04:32:04 -0800

    i feel like i am diagonally parked

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - main function
+ * @n:numbers
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
