c9e219ca4f52602ad673ed5cc0d7306c4da2fc5b
FrensiM <frensimeta@gmail.com>
2023-02-27 02:17:07 -0800

    reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - main func
+ * @n: number
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int _strlen(char *s)
+{
+	int len = 0;
+	int i;
+
+	for(i = 0; s[i] != '\0'; i ++)
+		len ++;
+	return len;
+}
