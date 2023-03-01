bdfd8a67e11e4354dfdafb6aa9a3de357967f882
FrensiM <frensimeta@gmail.com>
2023-03-01 06:14:33 -0800

    strcmp 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - compare
+ * @s1: s1
+ * @s2: s2
+ * Return: return the value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
