00e9c1d3d9da62328b40efeb0b505781f1024962
FrensiM <frensimeta@gmail.com>
2023-03-01 05:33:47 -0800

    2 strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- *
- *
+ * _strncpy - main funks
+ * @dest: destination
+ * @src: source
+ * @n: numbers
+ * Return: return value
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src [i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
