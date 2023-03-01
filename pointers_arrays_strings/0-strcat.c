0589aa582160ffa58f1ef16e7edc9f52d556956a
FrensiM <frensimeta@gmail.com>
2023-03-01 01:30:03 -0800

    0 strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,22 @@
 #include "main.h"
 /**
- *
- *
+ * _strcat - main func
+ * @src: src
+ * @dest: dest
+ * Return: return the value
  */
 char *_strcat(char *dest, char *src)
 {
+	char *ptr = dest;
 
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
+	return (dest);
 }
