5cc18c5eb1510390af197062a706d31bbd651009
FrensiM <frensimeta@gmail.com>
2023-03-02 04:21:15 -0800

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char *sep = ",;.!?\"(){} \n\t";
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] = str[0] - 32;
+	}
+	for (i = 0 ; str[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z' && str[i] == sep[j])
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+	}
+	return (str);
+}	
+
+
