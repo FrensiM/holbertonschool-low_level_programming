702a3c037b8ac3b3a899e8ad94ff63d89e555c41
FrensiM <frensimeta@gmail.com>
2023-03-02 14:40:58 -0800

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,39 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _atoi - main funk
+ * @s: num
+ * Return: return the value
+ */
 int _atoi(char *s)
 {
-	int n = 0;
-	int i = 0;
+	int len, i, sign, flag, j;
+	unsigned int num;
 
-	for (i = 0; s[i] != '\0'; i++)
+	len = num = flag = j = 0;
+	sign = 1;
+
+	while (s[len] != '\0')
+		len++;
+
+	if (len == 0)
+		return (0);
+
+	for (i = 0; i < len; i++)
 	{
-		printf("%d para mbledhjes\n", n);
-		n = n * 10 + s[i] - '0';
-		printf("%d pas mbledhjes\n", n);
+		if (s[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				num = num * 10 + (s[i] - '0');
+				i++;
+			}
+			return (sign * num);
+		}
 	}
-	return (n);
+	return (0);
 }
