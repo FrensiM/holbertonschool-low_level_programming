e0edd099c59201d648c6d99c04d0a7acac507be0
FrensiM <frensimeta@gmail.com>
2023-03-03 03:36:20 -0800

    strpbrk.c

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,28 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * print_array - print an array of integers
- * @a: an array of integers
- * @n: the number of elements to swap
- *
- * Return: nothing.
- */
-void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
 
 /**
  * main - check the code
@@ -31,10 +8,11 @@ void print_array(int *a, int n)
  */
 int main(void)
 {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
 
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
+    t = _strpbrk(s, f);
+    printf("%s\n", t);
     return (0);
 }
