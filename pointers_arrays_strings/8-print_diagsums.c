d0c189afb0a249dbabfb5b4240865dab56fd7be3
FrensiM <frensimeta@gmail.com>
2023-03-03 06:10:30 -0800

    print diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,14 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_diagsums - main fun
+ * @a: a
+ * @size: size
+ */
 void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + i*size + i);
-		sum2 += *(a + i*size + (size-i-1));
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - i - 1));
 	}
-	printf("%d\n", sum1);
-	printf("%d\n", sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
