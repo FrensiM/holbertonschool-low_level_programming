6f31283a03154ae46f384cf352e1abfcaeb80d04
FrensiM <frensimeta@gmail.com>
2023-03-03 05:49:45 -0800

    7 final

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i*size + i);
+		sum2 += *(a + i*size + (size-i-1));
+	}
+	printf("%d\n", sum1);
+	printf("%d\n", sum2);
+}
