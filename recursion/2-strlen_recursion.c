8ced50605c04545c93abb17c0fcd4e4b8e63428d
FrensiM <frensimeta@gmail.com>
2023-03-06 12:45:23 -0800

    strlen recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - main fun
+ * @s: char
+ * Return: return the value
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
