24e42591b5f2f63eb35fac611bf4c6700ffda0a3
FrensiM <frensimeta@gmail.com>
2023-03-08 02:17:19 -0800

    it aind that thry call you

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main fun
+ * @argc: arg
+ * @argv: argv
+ * Return: return the value
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
