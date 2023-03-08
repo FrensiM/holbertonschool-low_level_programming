9789b066244931771414b3d7146a31df66487865
FrensiM <frensimeta@gmail.com>
2023-03-08 02:30:34 -0800

    1. args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main fun
+ * @argc: argc
+ * @argv: argv
+ * Return: return the valeu
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
