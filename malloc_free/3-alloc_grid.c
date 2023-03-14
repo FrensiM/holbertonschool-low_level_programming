9462671cf8375c6b7c320481a06481553dc9ccfd
FrensiM <frensimeta@gmail.com>
2023-03-14 03:24:39 -0700

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,7 +13,8 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	array = malloc(sizeof(int) * height);
+
+	array = malloc(sizeof(int *) * height);
 
 	if (array == NULL)
 		return (NULL);
