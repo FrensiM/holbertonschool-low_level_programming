7ae64fa7ae7eb3c3a9ec8c563f95db13ac14c38c
FrensiM <frensimeta@gmail.com>
2023-03-20 03:12:41 -0700

    a dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "dog.h"
-
+/**
+ *
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		printf("OK");
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
