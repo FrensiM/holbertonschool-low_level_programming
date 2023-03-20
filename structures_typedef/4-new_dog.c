24a59156ba37db0c4d51b327d09a46816733231b
FrensiM <frensimeta@gmail.com>
2023-03-20 05:05:38 -0700

    new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ *
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
+		return (NULL);
+	newdog->name = malloc(strlen(name) + 1);
+	newdog->owner = malloc(strlen(owner) +1);
+	if ( !newdog->name || !newdog->owner)
+	{
+		free(newdog->name);
+		free(newdog->owner);
+		free(newdog);
+		return (NULL);
+	}
+
+	strcpy(newdog->name, name);
+	strcpy(newdog->owner, owner);
+
+	newdog->age = age;
+	
+	return (newdog);
+}
