9d1ea1f4365ec18d0661f11c666025fe53f1b49b
FrensiM <frensimeta@gmail.com>
2023-03-20 02:46:04 -0700

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct name
+ * @name: name
+ * @owner: owner
+ * @age: age
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
