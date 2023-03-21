19966bbc4095e7e6f78892c107a400797a17ef8b
FrensiM <frensimeta@gmail.com>
2023-03-21 05:58:42 -0700

    3 calc

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,49 @@
+#include "calc.h"
+/**
+ * op_add - main fun
+ * @a: num
+ * @b: num
+ * Return: return the add
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - main fun
+ * @a: num1
+ * @b: num2
+ * Return: return the sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mian funk
+ * @a: num1
+ * @b: num2
+ * Return: return the value
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - main fun
+ * @a: num1
+ * @b: num2
+ * Return: return teh value
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - main fun
+ * @a: num1
+ * @b: num2
+ * Return: return the value
+ */
+int op_mod(int a, int b)
+	return (a % b);
