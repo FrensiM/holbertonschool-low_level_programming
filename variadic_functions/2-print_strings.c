38b3ceabd847acbd8fbf033fcb7e7567c284a9e7
FrensiM <frensimeta@gmail.com>
2023-03-24 02:33:28 -0700

    strings print

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("nil");
 		else
 			printf("%s", ptr);
-		if (separator != NULL && i < n - 1)
+		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
