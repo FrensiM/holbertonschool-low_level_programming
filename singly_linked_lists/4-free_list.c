2f7fc93974586d4999a012cd0998aa9c9c1f3031
FrensiM <frensimeta@gmail.com>
2023-04-04 02:50:04 -0700

    free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * free_list - func free
+ * @head: head
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
