54b6c0f3c581ec04abc81c7534045f5433baf458
FrensiM <frensimeta@gmail.com>
2023-04-04 03:01:09 -0700

    free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -8,14 +8,13 @@
 void free_list(list_t *head)
 {
 	list_t *tmp;
-	
-	if(head == NULL)
-		free(head);
-	else
-		while (head != NULL)
-		{
-			tmp = head;
-			head = head->next;
-			free(tmp);
-		}
+
+	tmp = head;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
 }
