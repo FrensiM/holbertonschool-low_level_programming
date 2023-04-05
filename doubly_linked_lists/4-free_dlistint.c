387a9576ab48f0a097e3dcd34fbf30f410c31e33
FrensiM <frensimeta@gmail.com>
2023-04-05 01:13:21 -0700

    try

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	
+	while (current != NULL)
+	{
+		dlistint_t *next = current->next;
+		free(current);
+		current = next;
+	}
+}
