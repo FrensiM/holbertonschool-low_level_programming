7661e88b2f91db0d7982c50bca83838bbcc7a826
FrensiM <frensimeta@gmail.com>
2023-04-05 02:10:33 -0700

    tryy

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -8,11 +8,16 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
-
-	while (head != NULL && i < index)
-	{
-		head = head->next;
-		i++;
-	}
+	
+	if (head == NULL)
+		return (NULL);
+	else
+		while (head != NULL && i < index)
+		{
+			if (i > index)
+				printf("(nil)");
+			head = head->next;
+			i++;
+		}
 	return (head);
 }
