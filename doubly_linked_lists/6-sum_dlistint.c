d402e87611bcf7e7039a3a0b53a82c528b2850ac
FrensiM <frensimeta@gmail.com>
2023-04-05 02:53:09 -0700

    sum list

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -14,9 +14,9 @@ int sum_dlistint(dlistint_t *head)
 	if (head == NULL)
 		return (0);
 	while (head)
-		{
-			sum = sum + head->n;
-			head = head->next;
-		}
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
 	return (sum);
 }
