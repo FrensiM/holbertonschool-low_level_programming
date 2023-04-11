bc62216418e50521960570bccf2231a2adc9099b
FrensiM <frensimeta@gmail.com>
2023-04-11 03:34:38 -0700

    Tread lightly she is near

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * read_textfile - function that read a file
+ * @filename: file to read
+ * @letters: the number of letters it should read 
+ * Return: returns the actual number of letters it could read 
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, file_r, file_w;
+	char *txt;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	txt = malloc(sizeof(char) * letters);
+	if (txt == NULL)
+		return (0);
+	file_r = read(fd, txt, letters);
+	if (file_r == -1)
+		return (0);
+	file_w = write(STDOUT_FILENO, txt, file_r);
+	if (file_w == -1)
+		return (0);
+	close(fd);
+	return (file_r);
+}
---
+++ file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+void cp(char *file_from, char *file_to);
+void cls(int file);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+#endif
