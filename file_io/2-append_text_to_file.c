4161bcba835104b07da38806d387561a5ae77a61
FrensiM <frensimeta@gmail.com>
2023-04-12 02:10:12 -0700

    append a text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return(1);
+		return (1);
 	}
 	f_write = write(fd, text_content, strlen(text_content));
 
