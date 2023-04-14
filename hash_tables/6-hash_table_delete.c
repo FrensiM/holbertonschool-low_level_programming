39b536f0a475dc67d3c1758c389425f5dccb721d
FrensiM <frensimeta@gmail.com>
2023-04-14 05:33:42 -0700

    delete hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * hash_table_delete - get node
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *p, *temp;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		p = ht->array[i];
+		if (p)
+		{
+			temp = ht->array[i];
+			while (temp)
+			{
+				p = temp->next;
+				free(temp->key);
+				free(temp->value);
+				free(temp);
+				temp = p;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
