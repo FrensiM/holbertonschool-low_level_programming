a4d1d88e161b4c9a28de095a442dea68ed790075
FrensiM <frensimeta@gmail.com>
2023-05-22 02:04:01 -0700

    linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%li] = [%i]\n", i, array[i]);
 		if (array[i] == value)
-			return(i);
+			return (i);
 	}
 	return (-1);
 }
